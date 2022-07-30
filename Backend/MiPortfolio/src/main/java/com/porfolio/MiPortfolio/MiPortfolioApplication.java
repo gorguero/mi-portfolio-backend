f924016c093e07f5a06dd7d9c88bd1e0b2a65359
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-07-30 15:53:05 -0300

    Configuracion spring boot

---
+++ Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/MiPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.MiPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPortfolioApplication.class, args);
+	}
+
+}
---
+++ Backend/MiPortfolio/src/test/java/com/porfolio/MiPortfolio/MiPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.MiPortfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
