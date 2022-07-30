5054f62bf863b3f89542a3ff0ec982b814181cb0
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-07-30 17:37:32 -0300

    Cargando estructura de los repositories

--- Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/repository/ITecnologiaRepository.java
+++ Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/repository/ITecnologiaRepository.java
@@ -1,11 +1,11 @@
 
 package com.porfolio.MiPortfolio.repository;
 
-import com.porfolio.MiPortfolio.model.Educacion;
+import com.porfolio.MiPortfolio.model.Tecnologia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IEducacionRepository extends JpaRepository <Educacion, Long>{
+public interface ITecnologiaRepository extends JpaRepository <Tecnologia, Long>{
     
 }
