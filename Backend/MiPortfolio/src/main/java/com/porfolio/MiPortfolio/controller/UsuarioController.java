54d44d14f120ff5c473361c761f06c91aa46026f
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-07-30 23:49:03 -0300

    Ajuste

--- Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/UsuarioController.java
+++ Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/UsuarioController.java
@@ -2,7 +2,11 @@
 package com.porfolio.MiPortfolio.controller;
 
 import com.porfolio.MiPortfolio.interfaces.IUsuarioService;
+import com.porfolio.MiPortfolio.model.Usuario;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +15,10 @@ public class UsuarioController {
     @Autowired
     private IUsuarioService userService;
     
+    @GetMapping ("/usuario/obtener")
+    public List<Usuario> getUsuario(){
+        return userService.getUsuario();
+    }
     
     
 }
