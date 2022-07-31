d272c67b60b19b6395fddc8994cf810571db6975
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-07-30 23:41:14 -0300

    Creando controller para tecnologia

--- Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/TecnologiaController.java
+++ Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/TecnologiaController.java
@@ -1,13 +1,55 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.porfolio.MiPortfolio.controller;
 
-/**
- *
- * @author gorgu
- */
+import com.porfolio.MiPortfolio.interfaces.ITecnologiaService;
+import com.porfolio.MiPortfolio.model.Tecnologia;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TecnologiaController {
+    
+    @Autowired
+    private ITecnologiaService tecnologiaService;
+    
+    @GetMapping ("/tecnologia/obtener")
+    public List<Tecnologia> getTecnologia(){
+        return tecnologiaService.getTecnologia();
+    }
+    
+    @PostMapping ("/tecnologia/crear")
+    public String agregarTecnologia(@RequestBody Tecnologia tecnologia){
+        tecnologiaService.saveTecnologia(tecnologia);
+        return "Se ha creado correctamente";
+    }
+    
+    @DeleteMapping ("/tecnologia/borrar/{id}")
+    public String eliminarTecnologia(@PathVariable Long id){
+        tecnologiaService.deleteTecnologia(id);
+        return "Se ha eliminado correctamente";
+    }
+    
+    @PutMapping ("/tecnologia/editar/{id}")
+    public Tecnologia editarTecnologia(@PathVariable Long id,
+                                    @RequestParam ("nombre") String nuevoNombre,
+                                    @RequestParam ("link_logo") String nuevoLogo){
+        
+        Tecnologia tecnologia = tecnologiaService.findTecnologia(id);
+        
+        tecnologia.setNombre(nuevoNombre);
+        tecnologia.setLink_logo(nuevoLogo);
+        
+        tecnologiaService.saveTecnologia(tecnologia);
+        
+        return tecnologia;
+    }
     
 }
--- Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/UsuarioController.java
+++ Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/UsuarioController.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.porfolio.MiPortfolio.controller;
 
-/**
- *
- * @author gorgu
- */
+import com.porfolio.MiPortfolio.interfaces.IUsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
+    
+    @Autowired
+    private IUsuarioService userService;
+    
+    
     
 }
