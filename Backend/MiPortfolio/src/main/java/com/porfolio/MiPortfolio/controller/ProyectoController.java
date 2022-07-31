1aa4bf3adcdff86b7ec2772ffa300882e8090d18
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-07-30 23:28:46 -0300

    Creando controller para softskill

--- Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/ProyectoController.java
+++ Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/ProyectoController.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.porfolio.MiPortfolio.controller;
 
-/**
- *
- * @author gorgu
- */
+import com.porfolio.MiPortfolio.interfaces.IProyectoService;
+import com.porfolio.MiPortfolio.model.Proyecto;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProyectoController {
+    
+    @Autowired
+    private IProyectoService proyectoService;
+    
+    @GetMapping ("/proyecto/obtener")
+    public List<Proyecto> getProyecto(){
+        return proyectoService.getProyecto();
+    }
+    
+    @PostMapping ("/proyecto/crear")
+    public String agregarProyecto(@RequestBody Proyecto proyecto){
+        proyectoService.saveProyecto(proyecto);
+        return "Se ha creado correctamente";
+    }
+    
+    @DeleteMapping ("/proyecto/borrar/{id}")
+    public String eliminarProyecto(@PathVariable Long id){
+        proyectoService.deleteProyecto(id);
+        return "Se ha eliminado correctamente";
+    }
+    
+    @PutMapping ("/proyecto/editar/{id}")
+    public Proyecto editarProyecto(@PathVariable Long id,
+                                @RequestParam ("titulo") String nuevoTitulo,
+                                @RequestParam ("descripcion") String nuevoDescripcion,
+                                @RequestParam ("link_portada") String nuevoPortada){
+        
+        Proyecto proyecto = proyectoService.findProyecto(id);
+        
+        proyecto.setTitulo(nuevoTitulo);
+        proyecto.setDescripcion(nuevoDescripcion);
+        proyecto.setLink_portada(nuevoPortada);
+        
+        proyectoService.saveProyecto(proyecto);
+        
+        return proyecto;
+        
+    }
     
 }
