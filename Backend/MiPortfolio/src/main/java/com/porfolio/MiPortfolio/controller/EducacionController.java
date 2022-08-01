52e0ad2860dbd668aafcc76a29b6c18bc839c13d
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-08-01 13:06:00 -0300

    ajustes

--- Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/EducacionController.java
+++ Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/EducacionController.java
@@ -5,6 +5,7 @@ import com.porfolio.MiPortfolio.interfaces.IEducacionService;
 import com.porfolio.MiPortfolio.model.Educacion;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class EducacionController {
     
     @Autowired
@@ -62,7 +64,7 @@ public class EducacionController {
     }
     
     @GetMapping ("/educacion/encontrar/{id}")
-    public Educacion findUsuario(@PathVariable Long id){
+    public Educacion findEducacion(@PathVariable Long id){
         return educacionService.findEducacion(id);
     }
     
