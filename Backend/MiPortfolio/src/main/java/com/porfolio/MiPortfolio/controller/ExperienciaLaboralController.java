c1b8445e6c476d7558556d6c10fd487000806b1a
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-08-01 14:17:16 -0300

    Ajuste experiencia laboral controller

--- Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/ExperienciaLaboralController.java
+++ Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/controller/ExperienciaLaboralController.java
@@ -5,6 +5,7 @@ import com.porfolio.MiPortfolio.interfaces.IExperienciaLaboralService;
 import com.porfolio.MiPortfolio.model.ExperienciaLaboral;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class ExperienciaLaboralController {
     
     @Autowired
@@ -59,6 +61,11 @@ public class ExperienciaLaboralController {
         
         return expLaboral;
         
+    }
+    
+    @GetMapping ("/expLaboral/encontrar/{id}")
+    public ExperienciaLaboral findEducacion(@PathVariable Long id){
+        return expLaboralService.findExperienciaLaboral(id);
     }
     
 }
