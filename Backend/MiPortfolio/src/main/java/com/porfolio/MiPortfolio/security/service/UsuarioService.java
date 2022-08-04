cefd8ac23b87998fd0816fe801284da84d18e8f1
Juan Cruz Aguero Luna <jcaguerodev@gmail.com>
2022-08-04 10:29:36 -0300

    Parte 4.1
    Se ha creado el UsuarioService dentro de security

---
+++ Backend/MiPortfolio/src/main/java/com/porfolio/MiPortfolio/security/service/UsuarioService.java
@@ -0,0 +1,33 @@
+
+package com.porfolio.MiPortfolio.security.service;
+
+import com.porfolio.MiPortfolio.security.Entity.Usuario;
+import com.porfolio.MiPortfolio.security.repository.IUsuarioRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UsuarioService {
+    
+    @Autowired
+    IUsuarioRepository iUsuarioRepository;
+    
+    public Optional<Usuario> getByNickname(String nickname){
+        return iUsuarioRepository.findByNombreUsuario(nickname);
+    }
+    public boolean existByNickname(String nickname){
+        return iUsuarioRepository.existByNombreUsuario(nickname);
+    }
+    
+    public boolean existByEmail(String email){
+        return iUsuarioRepository.existByEmail(email);
+    }
+    
+    public void save(Usuario usuario){
+        iUsuarioRepository.save(usuario);
+    }
+    
+}
